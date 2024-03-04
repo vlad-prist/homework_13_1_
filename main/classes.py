42df854ac811ee610d7ee160f05213871edfd634
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-03-04 22:45:50 +0300

    Добавление новых методов в классы: @classmethod @property / getter / setter

--- main/classes.py
+++ main/classes.py
@@ -10,16 +10,27 @@ class Category:
         Товары передаются списком через метод добавления.'''
         self.name = name
         self.description = description
-        self.goods = []
+        self.__goods = [] #Список товаров сделали приватным
 
         Category.count_category += 1 #при создании экземлпяра, счетчик увеличивается на 1
 
+
     def list_goods(self, good):
         ''' Метод добавления товаров в список'''
-        self.goods.append(good)
+        self.__goods.append(good)
+
 
     def __repr__(self):
-        return f'{self.name}, {self.description}, {self.goods}'
+        return f'{self.name}, {self.description}, {self.__goods}'
+
+
+    @property
+    def get_format(self):
+        '''добавлен геттер для вывода необходимого формата'''
+        result = ''
+        for good in self.__goods:
+            result += f'{good.name}, {good.price} руб. Остаток: {good.quantity} шт.\n'
+        return result
 
 
 
@@ -27,36 +38,59 @@ class Product:
     name: str
     description: str
     price: float
-    amount: int
+    quantity: int
     count_product = 0 #Счетчик количества продуктов (экземпляров, не остатка)
+
 
     def __init__(self, name, description, price, quantity):
         '''Инициализация по заданию. Атрибуты экземпляра: наименование, описание, цена, остаток'''
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
         Product.count_product += 1 #при создании экземлпяра, счетчик увеличивается на 1
 
+
     def __repr__(self):
-        return f'{self.name}, {self.description}, {self.price}, {self.quantity}'
+        return f'{self.name}, {self.description}, {self.__price}, {self.quantity}'
+
+
+    @classmethod
+    def launch_product(cls, new_product):
+        '''Метод по добавлению новых товаров'''
+        name, price, description, quantity = new_product.split(',')
+        return cls(name, price, description, quantity)
+
+
+    @property
+    def price(self):
+        '''геттер - установка цены'''
+        return self.__price
+
+
+    @price.setter
+    def price(self, new_price):
+        '''сеттер - условие, если цена <= 0, то вывод сообщения, иначе вывод заданной цены'''
+        if new_price <= 0:
+            print('Введена некорректная цена')
+        else:
+            self.__price = new_price
 
 
 
-# ct1 = Category('Phones', 'mobile')
-# ct1.list_goods('iPhone')
-# ct1.list_goods('Samsung')
-#
-# ct2 = Category('Laptops', 'mini computers')
-# ct2.list_goods('iPhone')
-# ct2.list_goods('Samsung')
-# print(ct1)
-# print(ct2)
-# print(Category.count_category)
-#
-# pr1 = Product('iPhone', 'smth', 100_000, 3)
-# pr2 = Product('Samsung', 'smth', 90_000, 2)
-# print(pr1)
-# print(pr2)
-# print(Product.count_product)
+ct1 = Category('Phones', 'mobile')
+pr2 = Product('Samsung', 'smth', 90_000, 2)
+ct1.list_goods(pr2) #Проверка по заданию 2
+print(ct1.get_format) #Проверка по заданию 2
+
+pr1 = Product('iPhone', 'smth', 100_000, 3)
+pr3 = 'Nokia, smth, 1000, 10' #Проверка по заданию 3
+new_pr3 = Product.launch_product(pr3) #Проверка по заданию 3
+print(new_pr3) #Проверка по заданию 3
+
+pr1.price = 0 #Проверка по заданию 4
+print(pr1.price) #Проверка по заданию 4
+
+
+
